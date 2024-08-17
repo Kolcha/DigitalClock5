cbbc97f71102dfc2974d51c9a862dd36644e7b56
Nick Korotysh <nick.korotysh@gmail.com>
2024-08-17 07:50:57 +0200

    cosmetic changes

--- config/config_sections.hpp
+++ config/config_sections.hpp
@@ -16,7 +16,7 @@ public:
   CONFIG_OPTION_Q(bool, AppearancePerInstance, false)
 
   CONFIG_OPTION_Q(bool, StayOnTop, true)
-  CONFIG_OPTION_Q(bool, TransparentForMouse, false);
+  CONFIG_OPTION_Q(bool, TransparentForMouse, false)
 
   CONFIG_OPTION_Q(bool, CheckForUpdates, true)
   CONFIG_OPTION_Q(bool, CheckForBetaVersion, true)
@@ -51,23 +51,23 @@ public:
 
   CONFIG_OPTION_Q(int, Opacity, 80)
 
-  CONFIG_OPTION_Q(bool, UseFont, true);
-  CONFIG_OPTION_Q(QFont, Font, QFont("Comic Sans MS", 96));
-  CONFIG_OPTION_Q(QString, Skin, QString("electronic"));
+  CONFIG_OPTION_Q(bool, UseFont, true)
+  CONFIG_OPTION_Q(QFont, Font, QFont("Comic Sans MS", 96))
+  CONFIG_OPTION_Q(QString, Skin, QString("electronic"))
 
-  CONFIG_OPTION_Q(bool, SeparatorFlashes, true);
-  CONFIG_OPTION_Q(bool, UseCustomSeparators, false);
-  CONFIG_OPTION_Q(QString, CustomSeparators, QString());
+  CONFIG_OPTION_Q(bool, SeparatorFlashes, true)
+  CONFIG_OPTION_Q(bool, UseCustomSeparators, false)
+  CONFIG_OPTION_Q(QString, CustomSeparators, QString())
 
-  CONFIG_OPTION_Q(QString, TimeFormat, QString("HH:mm"));
+  CONFIG_OPTION_Q(QString, TimeFormat, QString("HH:mm"))
 
-  CONFIG_OPTION_Q(QBrush, Texture, QColor(112, 96, 240));
-  CONFIG_OPTION_Q(bool, TextureStretch, false);
-  CONFIG_OPTION_Q(bool, TexturePerCharacter, true);
+  CONFIG_OPTION_Q(QBrush, Texture, QColor(112, 96, 240))
+  CONFIG_OPTION_Q(bool, TextureStretch, false)
+  CONFIG_OPTION_Q(bool, TexturePerCharacter, true)
 
-  CONFIG_OPTION_Q(QBrush, Background, Qt::NoBrush);
-  CONFIG_OPTION_Q(bool, BackgroundStretch, false);
-  CONFIG_OPTION_Q(bool, BackgroundPerCharacter, false);
+  CONFIG_OPTION_Q(QBrush, Background, Qt::NoBrush)
+  CONFIG_OPTION_Q(bool, BackgroundStretch, false)
+  CONFIG_OPTION_Q(bool, BackgroundPerCharacter, false)
 };
 
 class SectionGeneric : public SettingsStorageClient {
