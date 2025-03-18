3261332e1697c7bc8c6dc2e859e65d63cefaaf8b
Nick Korotysh <nick.korotysh@gmail.com>
2025-03-18 21:02:03 +0100

    better bundle info in macOS plist
    - swapped "Display Name" and "Bundle Name" values
    - proper value for "Bundle Short Version String"
    - "Bundle Version" value is set to "build number"

--- cmake/clock_utils.cmake
+++ cmake/clock_utils.cmake
@@ -2,12 +2,14 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# define build number if we are building on GitHub
+set(BUILD_NUMBER 0)
+if (DEFINED ENV{GITHUB_RUN_NUMBER})
+  set(BUILD_NUMBER $ENV{GITHUB_RUN_NUMBER})
+endif()
+
 function(add_win_rc_file _target _file)
   if (WIN32)
-    set(BUILD_NUMBER 0)
-    if (DEFINED ENV{GITHUB_RUN_NUMBER})
-      set(BUILD_NUMBER $ENV{GITHUB_RUN_NUMBER})
-    endif()
     configure_file(${_file}.in ${_file} NEWLINE_STYLE CRLF)
     target_sources(${_target} PRIVATE
       ${CMAKE_CURRENT_BINARY_DIR}/${_file}
